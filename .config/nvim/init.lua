d29adaf70d1569706332015685ede0c923347548
luke-grantham <Luke@Lukes-MBP.attlocal.net>
2024-01-20 21:53:27 -0500

    first commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,44 @@
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.cursorline = true
+
+
+--
+-- lazy package manager: https://github.com/folke/lazy.nvim
+--
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+plugins = {
+    "folke/which-key.nvim",
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    }
+}
+
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+require("lazy").setup(plugins)
+--
+-- end package manager
+-- --
+
+
+-- Post Plugin Setup
+vim.cmd[[colorscheme tokyonight-moon]]
