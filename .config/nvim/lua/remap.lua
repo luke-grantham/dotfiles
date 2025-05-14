c4c8e8193e4db6a45dedf69b04a02e9521f2f58d
luke-grantham <lgrantham2@gmail.com>
2025-05-13 20:05:35 -0400

    Update remap.lua

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -1,13 +1,16 @@
-
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
---nnoremap("<C-d>","<C-d>zz")
---nnoremap("<C-u>","<C-u>zz")
---nnoremap("n","nzz")
---nnoremap("N","nzz")
 
 vim.keymap.set('n','<C-d>', '<C-d>zz')
 vim.keymap.set('n','<C-u>', '<C-u>zz')
 vim.keymap.set('n','n','nzz')
 vim.keymap.set('n','N','Nzz')
+
+-- telescope
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Live Grep" })
+
+-- make current file split bigger or smaller
+vim.keymap.set("n", "<leader>+", "10<C-w>+", { desc = "Make Split Wider" })
+vim.keymap.set("n", "<leader>-", "10<C-w>-", { desc = "Make Split Narrower" })
+
